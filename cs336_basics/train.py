5ada33e48c4077ef860634b400ef40f8f635a479
Sam <sam@samly.me>
2026-08-15 14:32:56 -0700

    optimize cross entropy

--- cs336_basics/train.py
+++ cs336_basics/train.py
@@ -12,16 +12,16 @@ import torch
 def cross_entropy(
     logits: Float[Tensor, "... vocab"],
     target: Int[Tensor, "... 1"],
-) -> Float[Tensor, "... vocab"]:
+) -> Float[Tensor, "1"]:
     # Subtract max for numerical stability
     maxes = reduce(logits, "... vocab -> ... 1", "max")
     logits = logits - maxes
 
     sum_exp = reduce(logits.exp(), "... vocab -> ... 1", "sum")
 
-    log_p = logits - sum_exp.log()
+    target_logits = logits.gather(dim=-1, index=target.unsqueeze(-1))
 
-    return -log_p.gather(dim=-1, index=target.unsqueeze(-1)).mean()
+    return -reduce(target_logits - sum_exp.log(), "... -> 1", "mean")
 
 
 class SGD(torch.optim.Optimizer):
