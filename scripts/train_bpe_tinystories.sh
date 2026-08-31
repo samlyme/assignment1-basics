f45b94ead67bf62bb12b938df4125f5cfeae25bc
sam <sam@samly.me>
2026-08-31 00:52:12 +0000

    update scripts

--- scripts/train_bpe_tinystories.sh
+++
@@ -1,1 +0,0 @@
-py-spy  record --subprocesses -o out/train_bpe_tinystories_profile.svg -- .venv/bin/python cs336_basics/train_tokenizer.py data/TinyStoriesV2-GPT4-train.txt --vocab-size 10000 --out-dir out/bpe_params_ts-train
