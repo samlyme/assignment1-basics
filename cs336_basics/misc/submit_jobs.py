042e8d84d7e5fce4e02b5e1f0f36c6bebfa3b664
sam <sam@samly.me>
2026-09-05 20:01:27 -0700

    refactor submission notebook

--- cs336_basics/misc/submit_jobs.py
+++ cs336_basics/misc/submit_jobs.py
@@ -22,53 +22,18 @@ import sympy as sp
 import torch
 from tqdm import tqdm
 
+
 # %%
-
-
-def submit_pueue(config: RunConfig) -> None:
-    def handle_path(value):
-        if isinstance(value, Path):
-            return str(value)
-        raise TypeError(
-            f"Object of type {type(value)} is not JSON serializable"
-        )
-
-    payload = json.dumps(asdict(config), default=handle_path)
-
-    command = shlex.join(
-        [
-            ".venv/bin/python",
-            "cs336_basics/train.py",
-            "--config",
-            payload,
-        ]
-    )
-
-    res = subprocess.run(
-        [
-            "pueue",
-            "add",
-            "--stashed",
-            "--",
-            command,
-        ],
-        check=True,
-        cwd="/home/sam/code/stanford-cs336/assignment1-basics",
-        capture_output=True,
-    )
-    print(res)
-
-
+# Approximate max batch size.
 train_path = Path("/data/tokenized/ts-train-10k_ts.npy")
 valid_path = Path("/data/tokenized/ts-valid-10k_ts.npy")
-# %%
 model_config = MODEL_CONFIGS["toy"]
 model_config_dict = asdict(model_config)
 keys = list(model_config_dict.keys())
 model_symbols = sp.symbols(keys)
 
 
-def sub(exp):
+def subs_model(exp):
     return exp.subs(
         {s: model_config_dict[k] for s, k in zip(model_symbols, keys)}
     )
@@ -135,11 +100,12 @@ VRAM = 24  # 24 GB for 3090
 peak_mem = (P + A + G + O) * 4 * 1e-9  # use fp32 = 4 bytes, 1e-9 is gb.
 # find upper bound for batch size for a give VRAM
 
-max_batch_size = int(sub(sp.solve(sp.Eq(peak_mem, VRAM), batch_size)[0]))
+max_batch_size = int(subs_model(sp.solve(sp.Eq(peak_mem, VRAM), batch_size)[0]))
 max_batch_size
 
 
 # %%
+# Approximate model memory usage for each batch size
 def batch_sizes_range(lo: int, hi: int) -> list[int]:
     out: list[int] = []
 
@@ -153,16 +119,16 @@ def batch_sizes_range(lo: int, hi: int) -> list[int]:
 
 batch_sizes = batch_sizes_range(1, max_batch_size)
 batch_sizes.reverse()  # for my setup it is better to run large batch sizes first.
+{b: subs_model(peak_mem.subs({batch_size: b})) for b in batch_sizes}
 
-# %%
-{b: sub(peak_mem.subs({batch_size: b})) for b in batch_sizes}
-# when benchmarking, this estimate is consistently too low!
 # %%
 # Batch size experiment with fixed total tokens processed
 total_tokens_processed = 327_680_000
 train_configs: list[TrainConfig] = []
 for batch_size in batch_sizes:
-    steps = int(sub(total_tokens_processed / batch_size / context_length))
+    steps = int(
+        subs_model(total_tokens_processed / batch_size / context_length)
+    )
     train_configs.append(
         TrainConfig(
             train=DatasetConfig(train_path),
@@ -174,43 +140,44 @@ for batch_size in batch_sizes:
 train_configs
 
 # %%
-# res = {}
-# data = np.load(
-#     "/data/tokenized/ts-valid-10k_ts.npy",
-#     mmap_mode="c",
-# )
-# min_lr = 1e-5
-# max_lr = 1e-2
-# for batch_size in tqdm(batch_sizes):
-#     model = model_from_config(model_config)
-#     lr = min_lr
-#     optimizer = AdamW(
-#         model.parameters(), lr=lr, betas=(0.9, 0.999), weight_decay=0
-#     )
+# LR sweep by batch size. Previously showed no real dependence.
+res = {}
+data = np.load(
+    "/data/tokenized/ts-valid-10k_ts.npy",
+    mmap_mode="c",
+)
+min_lr = 1e-5
+max_lr = 1e-2
+for batch_size in tqdm(batch_sizes):
+    model = model_from_config(model_config)
+    lr = min_lr
+    optimizer = AdamW(
+        model.parameters(), lr=lr, betas=(0.9, 0.999), weight_decay=0
+    )
 
-#     dataloader = torch.utils.data.DataLoader(
-#         NextTokenDataset(data, model_config.context_length),
-#         batch_size,
-#         shuffle=True,
-#     )
-#     lrs, losses = lr_sweep(model, optimizer, dataloader, device="cuda")
+    dataloader = torch.utils.data.DataLoader(
+        NextTokenDataset(data, model_config.context_length),
+        batch_size,
+        shuffle=True,
+    )
+    lrs, losses = lr_sweep(model, optimizer, dataloader, device="cuda")
 
-#     res[batch_size] = {"lrs": lrs, "losses": losses}
+    res[batch_size] = {"lrs": lrs, "losses": losses}
 
-# for batch_size, result in res.items():
-#     plt.plot(
-#         result["lrs"],
-#         result["losses"],
-#         label=f"BS={batch_size}",
-#     )
+for batch_size, result in res.items():
+    plt.plot(
+        result["lrs"],
+        result["losses"],
+        label=f"BS={batch_size}",
+    )
 
-# plt.xscale("log")
-# plt.xlabel("Learning rate")
-# plt.ylabel("Loss")
-# plt.title("Learning Rate Sweep by Batch Size")
-# plt.legend()
-# plt.grid(True, alpha=0.3)
-# plt.show()
+plt.xscale("log")
+plt.xlabel("Learning rate")
+plt.ylabel("Loss")
+plt.title("Learning Rate Sweep by Batch Size")
+plt.legend()
+plt.grid(True, alpha=0.3)
+plt.show()
 # seems like 1.5e-4 is very stable, with 1e-3 being relatively aggro for all batch sizes.
 
 # %%
@@ -219,6 +186,45 @@ train_configs
 optim_config = OptimizerConfig(lr=1.5e-4)
 
 # %%
+# Define function to submit jobs.
+
+
+def submit_pueue(config: RunConfig) -> None:
+    def handle_path(value):
+        if isinstance(value, Path):
+            return str(value)
+        raise TypeError(
+            f"Object of type {type(value)} is not JSON serializable"
+        )
+
+    payload = json.dumps(asdict(config), default=handle_path)
+
+    command = shlex.join(
+        [
+            ".venv/bin/python",
+            "cs336_basics/train.py",
+            "--config",
+            payload,
+        ]
+    )
+
+    res = subprocess.run(
+        [
+            "pueue",
+            "add",
+            "--stashed",
+            "--",
+            command,
+        ],
+        check=True,
+        cwd="/home/sam/code/stanford-cs336/assignment1-basics",
+        capture_output=True,
+    )
+    print(res)
+
+
+# %%
+# Submit batch size sweep.
 for train_config in train_configs:
     config = RunConfig(
         model=model_config,
